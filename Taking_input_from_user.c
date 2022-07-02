e33eafb8793b6a95e0d8c67ee9090e8bb965be8e
shankar dahal <86343933+shankardahal@users.noreply.github.com>
2022-07-02 16:33:44 +0545

    Add files via upload

---
+++ Taking_input_from_user.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+
+    int first;
+    int second;
+
+    printf("Enter The first number :\n");
+    scanf("%d", &first);
+
+    printf("Enter The second number :\n");
+    scanf("%d", &second);
+
+    int sum = first+second;
+    printf("The sum is : %d \n",sum);
+    
+
+
+    return 0;
+}
